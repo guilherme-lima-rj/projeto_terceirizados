7d7500a914d5021053d00b9d5da846b8710ca3e5
Guilherme Lima <guilherme.lima@ymail.com>
2024-07-24 18:50:05 -0300

    subindo o codigo da coleta com atualizacao de comentarios no codigo.

--- coleta/coleta/spiders/terceirizados.py
+++ coleta/coleta/spiders/terceirizados.py
@@ -7,27 +7,24 @@ class TerceirizadosSpider(scrapy.Spider):
     start_urls = ["https://www.gov.br/cgu/pt-br/acesso-a-informacao/dados-abertos/arquivos/terceirizados"]
 
     def parse(self, response):
-        #Seleciona os paths dos arquivos XLSX
+        #Seleciona os paths dos arquivos CSV e XLSX
         paths = response.xpath('//a[contains(@href, ".csv") or contains(@href, ".xlsx")]/@href').extract()
 
+        # Para cada item da lista, cria uma requisição para baixar o arquivo da vez
         for path in paths:        
             yield scrapy.Request(url=response.urljoin(path), callback=self.download_file)
-
-        # response.css('div.tile.tile-default') 
-        #xpath('//a/@href').extract().re(r'.*\.xlsx$')
-        
-        #csv_links = response.css('a::attr(href)').re(r'.*\.csv$')
-        #for link in csv_links:
-        #    yield scrapy.Request(response.urljoin(link), self.save_csv)
-
 
     def download_file(self, response):
         # Definindo diretório onde os arquivos serão salvos
         download_dir = '../dados'
+
+        # Cria o diretório, caso não exista
         os.makedirs(download_dir, exist_ok=True)
 
         # Captura o nome do arquivo 
         file_name = os.path.join(download_dir, response.url.split("/")[-1])
+
+        # Salva o arquivo no diretório acima especificado
         with open(file_name, 'wb') as f:
             f.write(response.body)
         self.log(f'Salvo arquivo {file_name}')
